623408cca58343555fa5700db11658215a4348c3
Victor <victorhenriquesan@outlook.com>
2025-12-31 21:08:53 -0300

    Preparando ambiente

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+<div>
+    @extends('layouts.addBootstrap')
+    @section('content')
+        <h1 class="text-primary">Gerenciador de Hábitos - Ajustes iniciais do projeto!</h1>
+    @endsection
+</div>
